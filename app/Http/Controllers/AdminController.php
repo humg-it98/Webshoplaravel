e69ef5e83d3713ec24e051a338fab6e4bc47200d
humg-it98 <thoigian5792@gmail.com>
2021-04-22 16:24:48 +0700

    AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function AuthLogin(){
+        $admin_id = Session::get('admin_id');
+        if('admin_id'){
+            return Redirect::to('dashboard');
+        }else{
+            return Redirect::to('admin')->send();
+        }
+    }
+
+    public function index(){
+        return view('admin_login');
+    }
+    public function show_dashboard(){
+        return view('admin_layout');
+    }
+    public function dashboard(Request $request){
+        $admin_email = $request->admin_email;
+        $admin_password = md5($request->admin_password);
+
+        $result = DB::table('tbl_admin')->where('admin_email',$admin_email)->where('admin_password',$admin_password)->first();
+
+
+        if($result){
+            Session::put('admin_name',$result->admin_name);
+            Session::put('admin_id',$result->admin_id);
+            return view('admin.dashboard');
+        }else
+        {
+            Session::put('message','Tai khoan hoac mat khau bi sai, vui long nhap lai');
+            return Redirect::to('/admin');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','HomeController@index');
 Route::get('/trang-chu','HomeController@index');
 
-Route::get('/admin','HomeController@index');
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
+Route::get('/logout','AdminController@logout');
 
