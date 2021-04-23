5c100623d8b798c4e99296ae242c3481b5d51ff9
humg-it98 <thoigian5792@gmail.com>
2021-04-23 13:45:32 +0700

    code database Brand_product && add-edit-delete BP

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*Front-end*/
 Route::get('/','HomeController@index');
 Route::get('/trang-chu','HomeController@index');
 
+/*Back-end*/
 Route::get('/admin','AdminController@index');
 Route::get('/dashboard','AdminController@show_dashboard');
 Route::post('/admin-dashboard','AdminController@dashboard');
 
 Route::get('/logout','AdminController@logout');
+
+//Category Product
+Route::get('/add-category-product','CategoryProduct@add_category_product');
+Route::get('/all-category-product','CategoryProduct@all_category_product');
+
+Route::get('/inactive-category-product/{category_product_id}','CategoryProduct@inactive_category_product');
+Route::get('/active-category-product/{category_product_id}','CategoryProduct@active_category_product');
+Route::get('/edit-category-product/{category_product_id}','CategoryProduct@edit_category_product');
+Route::post('/update-category-product/{category_product_id}','CategoryProduct@update_category_product');
+Route::get('/delete-category-product/{category_product_id}','CategoryProduct@delete_category_product');
+Route::post('/save-category-product','CategoryProduct@save_category_product');
+
+//Brand product
+Route::get('/add-brand-product','BrandProduct@add_brand_product');
+Route::get('/all-brand-product','BrandProduct@all_brand_product');
+Route::get('/inactive-brand-product/{brand_product_id}','BrandProduct@inactive_brand_product');
+Route::get('/active-brand-product/{brand_product_id}','BrandProduct@active_brand_product');
+Route::get('/edit-brand-product/{brand_product_id}','BrandProduct@edit_brand_product');
+Route::post('/update-brand-product/{brand_product_id}','BrandProduct@update_brand_product');
+Route::get('/delete-brand-product/{brand_product_id}','BrandProduct@delete_brand_product');
+Route::post('/save-brand-product','BrandProduct@save_brand_product');
+
+
 
