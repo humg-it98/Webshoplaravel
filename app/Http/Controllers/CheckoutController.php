111970287eaf933f499ece542a46a27ccfe728f9
humg-it98 <thoigian5792@gmail.com>
2021-05-03 15:04:15 +0700

    Create table SQL cart-shipping-payment-ordercart

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Cart;
+use Session;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+session_start();
+
+class CheckoutController extends Controller
+{
+    public function login_checkout(){
+        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+        $brand_product = DB::table('tbl_brand_product')->where('brand_status','0')->orderby('brand_id','desc')->get();
+        return view('pages.checkout.login_checkout')->with('category',$cate_product)->with('brand',$brand_product);
+    }
+    public function add_customer(Request $request){
+        $data = array();
+        $data['customer_name'] = $request->customer_name;
+        $data['customer_phone'] = $request->customer_phone;
+        $data['customer_email'] = $request->customer_email;
+        $data['customer_password'] = md5($request->customer_password);
+
+        $customer_id = DB::table('tbl_customers')->insertGetId($data);
+
+        Session::put('customer_id',$customer_id);
+        Session::put('customer_name',$request->customer_name);
+        return Redirect::to('/checkout');
+
+    }
+    public function checkout(){
+        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+        $brand_product = DB::table('tbl_brand_product')->where('brand_status','0')->orderby('brand_id','desc')->get();
+        return view('pages.checkout.show_checkout')->with('category',$cate_product)->with('brand',$brand_product);
+    }
+    public function save_checkout_customer(Request $request){
+        $data = array();
+        $data['shipping_name'] = $request->shipping_name;
+        $data['shipping_address'] = $request->shipping_address;
+        $data['shipping_phone'] = $request->shipping_phone;
+        $data['shipping_email'] = $request->shipping_email;
+        $data['shipping_note'] = $request->shipping_note;
+
+
+        $shipping_id = DB::table('tbl_shipping')->insertGetId($data);
+
+        Session::put('shipping_id',$shipping_id);
+        return Redirect::to('/payment');
+    }
+    public function payment(){
+        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+        $brand_product = DB::table('tbl_brand_product')->where('brand_status','0')->orderby('brand_id','desc')->get();
+        return view('pages.checkout.payment')->with('category',$cate_product)->with('brand',$brand_product);
+    }
+    public function logout_checkout(){
+       Session::flush();
+       return Redirect::to('/login-checkout');
+    }
+    public function login_customer(Request $request){
+        $customer_email = $request->user_email;
+        $customer_password = md5($request->user_password);
+
+        $result = DB::table('tbl_customer')->where('customer_email',$customer_email)->where('customer_password',$customer_password)->first();
+
+
+        if($result){
+            Session::put('customer_name',$result->customer_name);
+            Session::put('customer_id',$result->customer_id);
+            return Redirect::to('/checkout');
+        }else
+        {
+            Session::put('message','Tai khoan hoac mat khau khách hàng bi sai, vui long nhap lai');
+            return Redirect::to('/login-checkout');
+        }
+     }
+    public function order_place(Request $request){
+        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+        $brand_product = DB::table('tbl_brand_product')->where('brand_status','0')->orderby('brand_id','desc')->get();
+        //insert payment-method
+        $data = array();
+        $data['payment_method'] = $request->payment_option;
+        $data['payment_status'] = 'Đang chờ xử lý';
+        $payment_id = DB::table('tbl_payment')->insertGetId($data);
+
+         //insert order
+        $order_data = array();
+        $order_data['customer_id'] = Session::get('customer_id');
+        $order_data['shipping_id'] = Session::get('shipping_id');
+        $order_data['payment_id'] = $payment_id;
+        $order_data['order_total'] = Cart::total();
+        $order_data['order_status'] = 'Đang chờ xử lý';
+        $order_id = DB::table('tbl_order')->insertGetId($order_data);
+
+        //insert order_details
+        $content = Cart::content();
+        foreach($content as $v_content){
+          $order_d_data['order_id'] = $order_id;
+          $order_d_data['product_id'] = $v_content->id;
+          $order_d_data['product_name'] = $v_content->name;
+          $order_d_data['product_price'] = $v_content->price;
+          $order_d_data['product_sales_quantity'] = $v_content->qty;
+          DB::table('tbl_order_details')->insert($order_d_data);
+        }
+        if($data['payment_method']==1){
+            Cart::destroy();
+            $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+            $brand_product = DB::table('tbl_brand_product')->where('brand_status','0')->orderby('brand_id','desc')->get();
+            return view('pages.checkout.handcash')->with('category',$cate_product)->with('brand',$brand_product);
+
+
+        }elseif($data['payment_method']==2){
+            echo 'Xin lỗi tôi chưa làm chức năng này';
+
+        }else{
+          echo 'Thẻ ghi nợ';
+
+        }
+        //return Redirect::to('/payment');
+    }
+    public function manage_order(){
+        $all_order = DB::table('tbl_order')
+        ->join('tbl_customers','tbl_order.customer_id','=','tbl_customers.customer_id')
+        ->select('tbl_order.*','tbl_customers.customer_name')
+        ->orderby('tbl_order.order_id','desc')->get();
+    	$manager_order  = view('admin.manage_order')->with('all_order',$all_order);
+    	return view('admin_layout')->with('admin.manage_order', $manager_order);
+
+    }
+    public function view_order(){
+        $order_by_id = DB::table('tbl_order')
+        ->join('tbl_customers','tbl_order.customer_id','=','tbl_customers.customer_id')
+        ->join('tbl_shipping','tbl_order.shipping_id','=','tbl_shipping.shipping_id')
+        ->join('tbl_order_details','tbl_order_details.order_id','=','tbl_order.order_id')
+        ->select('tbl_order.*','tbl_customers.*','tbl_shipping.*','tbl_order_details.*')->first();
+        //dd($order_by_id);
+    	$manager_order_by_id  = view('admin.view_order')->with('order_by_id',$order_by_id);
+    	return view('admin_layout')->with('admin.view_order', $manager_order_by_id);
+
+    }
+}
