37c8d4ce2b556a1be0e5711becb78fceee86dd27
humg-it98 <thoigian5792@gmail.com>
2021-05-14 08:34:47 +0700

    tạo PartnerController

---
+++ resources/views/partner.blade.php
@@ -0,0 +1,13 @@
+@section('partner')
+    <div class="heading-part mb_10 ">
+        <h2 class="main_title">Đối tác liên kết</h2>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+        <div class="brand owl-carousel ptb_20">
+            <div class="item text-center"> <a href="#"><img src="images/brand/brand1.png" alt="Disney" class="img-responsive" /></a> </div>
+        </div>
+        </div>
+    </div>
+    </div>
+@endsection
